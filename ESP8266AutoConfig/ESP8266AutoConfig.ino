536aa128355427c449b96d8ea3966c59408dd89d
Charlee <oda.charlee@gmail.com>
2022-01-03 22:54:23 -0500

    initial commit

---
+++ ESP8266AutoConfig/ESP8266AutoConfig.ino
@@ -0,0 +1,26 @@
+#include "ESP8266AutoConfig.h"
+
+#define RECONFIG_PIN 4
+
+void setup() {
+
+  // initialize serial debug port
+  Serial.begin(115200);
+  Serial.println();
+
+  // connect IO4 with GND to reconfig WiFi
+  pinMode(RECONFIG_PIN, INPUT_PULLUP);
+
+  int reconfig = digitalRead(RECONFIG_PIN);
+  if (reconfig == LOW) {
+    ESP8266AutoConfig.startAp(true);
+  } else {  
+    // starting WiFi
+    ESP8266AutoConfig.connectWiFi(true);
+  }
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  ESP8266AutoConfig.poll();
+}
